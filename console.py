a787385dd8afa3dd333e86844fedf54a7a75bb3e
elasri21 <islame.edu@gmail.com>
2024-01-16 07:05:48 -0800

    ts console

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,34 +115,26 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        try:
-            if not args:
-                raise SyntaxError()
-            arg_l = args.split(" ")
-            keyw = {}
-            for item in arg_l[1:]:
-                splited = item.split("=")
-                splited[1] = eval(splited[1])
-                if type(splited[1]) is str:
-                    splited[1] = splited[1].replace("_", " ").replace('"', '\\"')
-                keyw[splited[0]] = splited[1]
-        except SyntaxError:
+        a_list = args.split(' ')
+        if not args:
             print("** class name missing **")
-        except NameError:
+            return
+        elif a_list[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        new_ins = HBNBCommand.classes[arg_l[0]](**keyw)
-        new_ins.save()
+            return
+        new_ins = HBNBCommand.classes[a_list[0]]()
         print(new_ins.id)
-        """if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()"""
+        for params in a_list[1:]:
+            key_value = params.split('=')
+            key = key_value[0]
+            value = key_value[1]
+            table = {
+                34: None,
+                95: 32
+            }
+            value = value.translate(table)
+            setattr(new_ins, key, value)
+        new_ins.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -290,7 +282,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -298,10 +290,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
