f434dc122819b7052d1c0271823e16b21c552317
elasri21 <islame.edu@gmail.com>
2024-01-31 11:41:44 -0800

    st 01

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,8 +13,8 @@ def do_pack():
         date_format = '%Y%m%d%H%M%S'
         arch_path = 'versions/web_static_{}.tgz'.format(
             datetime.now().strftime(date_format))
-        local('tar -cvzf {} web_static'.format(date_format))
+        local('tar -cvzf {} web_static'.format(arch_path))
         print('web_static packed:{} -> {}'.format(arch_path,
             os.path.getsize(arch_path)))
-    except:
+    except TypeError:
         None
