3d56736497556c63975fab3cac3dc75f0d4e89e5
elasri21 <islame.edu@gmail.com>
2024-01-31 11:35:23 -0800

    st 01

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that generates a .tgz archive from the contents
+of the web_static folder"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """generates a .tgz archive"""
+    try:
+        local('mkdir -p versions')
+        date_format = '%Y%m%d%H%M%S'
+        arch_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(date_format))
+        local('tar -cvzf {} web_static'.format(date_format))
+        print('web_static packed:{} -> {}'.format(arch_path,
+            os.path.getsize(arch_path)))
+    except:
+        None
