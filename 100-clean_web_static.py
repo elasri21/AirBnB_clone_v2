6e509edb81e218f4ec720d41862565ae1236215b
elasri21 <islame.edu@gmail.com>
2024-02-04 11:54:17 -0800

    ts

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,12 +10,18 @@ env.hosts = ['34.207.120.230', '100.25.138.49']
 def do_clean(number=0):
     """Delete out-of-date archives.
     Args:
-        number (int): The number of archives to keep."""
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
     number = 1 if int(number) == 0 else int(number)
+
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archives]
+
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
