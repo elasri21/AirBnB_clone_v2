a6eb8fe98078c546a5740e2f73a5ce49cbdfa61a
elasri21 <islame.edu@gmail.com>
2024-02-18 01:03:41 -0800

    ts

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,19 +6,18 @@ from models import storage
 app = Flask(__name__)
 
 
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """handles tearing down of db"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page: (inside the tag BODY"""
     states = storage.all("State")
     sorted_states = sorted(states.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """handles tearing down of db"""
+    storage.close()
 
 
 if __name__ == '__main__':
