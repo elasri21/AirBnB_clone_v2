00a084ef9a96dd5a3cf5c01ccfc18d64998e58e8
elasri21 <islame.edu@gmail.com>
2024-02-18 01:59:37 -0800

    ts

--- models/state.py
+++ models/state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from os import getenv
 from sqlalchemy.orm import relationship
 import models
+from models.city import City
 
 
 class State(BaseModel, Base):
